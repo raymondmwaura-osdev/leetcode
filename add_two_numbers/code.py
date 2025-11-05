32152991c74dafc1ed12759453e92a7feed1ccde
raymondmwaura-osdev <raymondmwaura232@gmail.com>
2025-11-05 18:04:22 +0300

    Polished.

--- add_two_numbers/code.py
+++ add_two_numbers/code.py
@@ -3,61 +3,24 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        self.l1 = l1
-        self.l2 = l2
-
-        result_list = []
+        dummy = ListNode()  # placeholder head
+        current = dummy
         carry = 0
 
-        while True:
-            try:
-                try:
-                    l1_num = self.l1.val
+        while l1 or l2 or carry:
+            val1 = l1.val if l1 else 0
+            val2 = l2.val if l2 else 0
 
-                except Exception as e:
-                    if carry == 1: l1_num = 0
-                    else: break
+            total = val1 + val2 + carry
+            carry = total // 10
+            current.next = ListNode(total % 10)
+            current = current.next
 
-                try:
-                    l2_num = self.l2.val
+            # move to next nodes if they exist
+            if l1: l1 = l1.next
+            if l2: l2 = l2.next
 
-                except Exception as e:
-                    l2_num = 0
-
-                result = l1_num + l2_num + carry
-
-                if result >= 10:
-                    stored_result = result - 10
-                    result_list.append(stored_result)
-                    carry = 1
-                
-                else:
-                    result_list.append(result)
-                    carry = 0
-                
-                self.l1 = self.l1.next
-
-                try:
-                    self.l2 = self.l2.next
-
-                except Exception as e:
-                    pass
-
-            except AttributeError:
-                break
-        
-        node_list = []
-        for num in result_list:
-            node_list.append(
-                ListNode(val=num, next=None)
-                )
-
-        list_length = len(node_list)
-        if list_length > 1:
-            for index in range(list_length - 1):
-                node_list[index].next = node_list[index + 1]
-
-
-        return node_list[0]
+        return dummy.next
